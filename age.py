a2dad456dbdd4624c9f8fa27afa856bed250d209
ant104247 <31909932+ant104247@users.noreply.github.com>
2018-01-07 20:25:35 +0800

    add age.py & bmi.py

--- age.py
+++ age.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-name = raw_input("what is your name?")
-birthdate = raw_input("what is your birthdate")
+# raw input is always string ...
 
+name = raw_input("what is your name?")
+birthdate = raw_input("what is your birthdate ")
+
+# python will take care of input and set type accordingly......
 age = input("How old are you?")
 
 print("%s was born on %s" % (name, birthdate))
